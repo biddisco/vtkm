91fba31f9f44901f302d9651626f161dfa2fa9a4
John Biddiscombe <biddisco@cscs.ch>
2015-04-22 09:50:44 +0200

    Dummy compositor test

---
+++ vtkmtest/compositor_hpx.cpp
@@ -0,0 +1,21 @@
+
+// tell vtkm that we want to use HPX backend
+#define VTKM_DEVICE_ADAPTER VTKM_DEVICE_ADAPTER_HPX
+
+// squash windows #defines before #including hpx
+#define NOMINMAX
+
+// override int main() so that hpx is initialized on startup
+#include <hpx/hpx_main.hpp>  
+
+// squash windows #defines FTER #INCLUDING HPX
+#undef GetMessage
+
+// if we have FreeGlut, change event handler
+// #ifdef VTKM_USE_FREEGLUT
+#undef VTKM_USE_FREEGLUT
+
+#define HPX_TIMING
+
+#include <vtkmtest/compositor.cpp>
+
